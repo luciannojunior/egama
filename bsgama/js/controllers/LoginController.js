8a5c0ff70d8d00915571a6baff6bc473e2537f32
Luciano Junior <luciannojunior@gmail.com>
2016-04-04 13:30:58 -0300

    pagina de login. ainda falta terminar a logica de login

---
+++ bsgama/js/controllers/LoginController.js
@@ -0,0 +1,35 @@
+cgama.controller('LoginController', ['User', '$window',  function (User, $window    ) {
+    
+      var self = this;
+      
+      self.usuario = {};
+      self.erro = false;
+      self.sucesso = false;
+      self.loading = false;
+      
+      self.login = function (usuario) {
+          
+          if (typeof usuario === 'undefined'){
+              self.erro = true;
+              return;
+          }
+          
+          self.loading = true;
+          
+          User.login(usuario.login, usuario.password).then(
+              function (data) {
+                  console.log(data);
+                  self.loading = false;
+                  self.sucesso = true;
+                  $window.location.href = "index.html";
+              },
+              function (err) {
+                  console.log(err);
+                  self.erro = true;
+                  usuario = {};
+                  self.loading = false;
+              }
+          );
+      };
+      
+}]);
