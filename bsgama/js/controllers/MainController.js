249ec06093cdf57fb44bb198bfb6cfcc47aa57b0
Luciano Junior <luciannojunior@gmail>
2016-04-08 14:26:34 -0300

    View do main no frontend

--- bsgama/js/controllers/MainController.js
+++ bsgama/js/controllers/MainController.js
@@ -1,5 +1,13 @@
 cgama.controller('MainController', ['$stateParams','User', function ($stateParams, User) {
     this.titulo = $stateParams.id;
-    
-      
+    // TODO: Encapsular manipulação de datas em um Factory
+    function dataBR() {
+        var d = new Date();
+        var meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro' ];
+        var dias = ['Segunda-Feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-Feira', 'Sábado', 'Domingo'];
+        var output = "Hoje é "+dias[d.getDay()-1]+", ";
+        output+= d.getDate().toString() + " de " + meses[d.getMonth()] + " de " + d.getFullYear().toString();
+        return output;
+    }
+    this.date = dataBR();
 }]);
